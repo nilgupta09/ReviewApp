fff30a25f498c6a1ffb80f3df34b3e130b940f77
niles <niles@AVIKA>
2023-10-24 22:19:00 -0400

    Many-to-Many RelationshipAdd project files.

---
+++ ReviewApp/Interfaces/IPokemonRepository.cs
@@ -0,0 +1,10 @@
+﻿using ReviewApp.Models;
+
+namespace ReviewApp.Interfaces
+{
+    public interface IPokemonRepository
+    {
+        ICollection<Pokemon> GetPokemons();
+
+    }
+}
---
+++ ReviewApp/Repository/PokemonRepository.cs
@@ -0,0 +1,20 @@
+﻿
+using ReviewApp.Data;
+using ReviewApp.Models;
+
+namespace ReviewApp.Repository
+{
+    public class PokemonRepository
+    {
+        private readonly DataContext _context;
+        public PokemonRepository(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemons.OrderBy(p =>p.Id).ToList();
+        }
+    }
+}
